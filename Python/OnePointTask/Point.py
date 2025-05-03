9d67e55386d3721b68af7b7540745d3fc36b81ad
Samuel Ferreira <75032911+SamuelLindao@users.noreply.github.com>
2025-05-03 18:46:22 -0300

    Python Code - Start to Dad and Children
    Python code that had one point reward I did get it :)

---
+++ Python/OnePointTask/Point.py
@@ -0,0 +1,29 @@
+from os import replace
+
+
+class SensorBase:
+    def coletar_dados(self):
+        return "Coletando dados genericos\n"
+
+class SensorTemperatura(SensorBase):
+    def coletar_dados(self):
+        dados = super().coletar_dados()
+        return dados + "Temperatura 22^C\n"
+
+class SensorUmidade(SensorBase):
+    def coletar_dados(self):
+        dados = super().coletar_dados()
+        return dados + "Umidade 45%\n"
+
+class SensorComposto(SensorTemperatura, SensorUmidade):
+    def coletar_dados(self):
+        dados = super(SensorTemperatura, self).coletar_dados()
+        dadosnovo = super(SensorUmidade, self).coletar_dados()
+        return dados+ "\n" + dadosnovo
+
+
+
+dados = SensorTemperatura
+value = dados.coletar_dados()
+value = value.replace( "Coletando dados genericos\n", "")
+print(value)
