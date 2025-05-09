c32a8a734a6a09f860d947b3356c789a359eef1f
Samuel Ferreira <75032911+SamuelLindao@users.noreply.github.com>
2025-05-09 10:47:31 -0300

    UPDATE

--- Python/OnePointTask/LinkedList.py
+++ Python/OnePointTask/LinkedList.py
@@ -36,10 +36,42 @@ class InsertNode(Node):
         while current:
             if current != self.last:
                 current = current.next
+            else:
+                break
         current.next = Node(data, current.next, self.last)
         self.last = current
 
 class DeleteNode(Node):
+    def __init__(self, first: Node, last:Node, data, prev:Node, next:Node):
+        super().__init__(data, prev, next)
+        self.first = first
+        self.last = last
+        last.next = first
+        first.prev = last
+
+
+    def delete(self):
+        if self.last.prev:
+            self.last.prev.next = self.last.next
+        self.last = self.last.prev
+        return self.last.prev
+
+class PrintNode(Node):
+    def __init__(self, first: Node, last:Node, data, prev:Node, next:Node):
+        super().__init__(data, prev, next)
+        self.first = first
+        self.last = last
+        last.next = first
+        first.prev = last
+
+    def print_list(self):
+        current_node = self
+        while current_node != self.last:
+            print(current_node.data)
+            current_node = current_node.next
+
+
+class ComboNode(InsertNode, DeleteNode, PrintNode):
     def __init__(self, first, last, data, prev, next):
         super().__init__(data, prev, next)
         self.first = first
@@ -47,5 +79,19 @@ class DeleteNode(Node):
         last.next = first
         first.prev = last
 
-        
-    def delete(self):
+# Cria nó inicial com valor 1 (lista circular: 1 <-> 1)
+node = ComboNode(1)
+
+# Insere 2 após o nó 1 (lista: 1 <-> 2 <-> 1)
+new_node = node.insert_after(2)
+
+# Insere 3 após o nó 2 (lista: 1 <-> 2 <-> 3 <-> 1)
+new_node.insert_after(3)
+
+# Imprime a lista a partir do nó 1
+node.print_list()
+# Saída esperada: 1 <-> 2 <-> 3 <-> 1
+
+# Deleta o nó 2 (lista: 1 <-> 3 <-> 1)
+deleted_prev = new_node.delete()
+deleted_prev.print_list()
