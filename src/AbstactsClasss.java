59a2f5427896322b0fb630f7c2883cdf0a893ba8
SamuelLindao <lacerdasamuel26@gmail.com>
2025-05-15 21:44:38 -0300

    Added an abstract class for study.

---
+++ src/AbstactsClasss.java
@@ -0,0 +1,15 @@
+﻿import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstactsClasss {
+
+    List<Integer> ints = new ArrayList<Integer>();
+
+    public abstract void insertData(int Data);
+    public abstract void removeData(int Data);
+    public void PrintSList()
+    {
+        for(int i =0 ; i < ints.size() ; i++) System.out.println(ints.get(i));
+    }
+}
+
