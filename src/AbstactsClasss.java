9eb97863e24aaad027082048ddb983ed1a6e828b
SamuelLindao <lacerdasamuel26@gmail.com>
2025-05-15 22:00:11 -0300

    Implemeted abstract class and an use

--- src/AbstactsClasss.java
+++ src/AbstactsClasss.java
@@ -12,4 +12,22 @@ public abstract class AbstactsClasss {
         for(int i =0 ; i < ints.size() ; i++) System.out.println(ints.get(i));
     }
 }
+class ChildClass extends  AbstactsClasss
+{
+
+    @Override
+    public void insertData(int Data) {
+
+    }
+
+    @Override
+    public void removeData(int Data) {
+
+    }
+
+    @Override
+    public void PrintSList() {
+
+    }
+}
 
