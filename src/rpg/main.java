6af9c5b8eaa5e36ad043250ab13400f062be8539
SamuelLindao <lacerdasamuel26@gmail.com>
2025-05-27 10:32:29 -0300

    class test

--- src/rpg/main.java
+++ src/rpg/main.java
@@ -3,6 +3,7 @@
 public class main {
     public static void main(String[] args) {
         Batalha batalha = new Batalha();
-
+        Ladino ladino = new Ladino("Samuel", 10, 10, false);
+        batalha.adicionar_aliado(ladino);
     }
 }
