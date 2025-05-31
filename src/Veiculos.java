ba097718c769293b1997183f94433ce3dec0131f
SamuelLindao <lacerdasamuel26@gmail.com>
2025-05-30 21:38:43 -0300

    car class

--- src/Veiculos.java
+++ src/Veiculos.java
@@ -1,4 +1,4 @@
-﻿public class Veiculos {
+﻿public abstract class Veiculos {
     String Tipo;
     public Veiculos(String Tipo)
     {
@@ -6,6 +6,11 @@
     }
     public void ImprimirTipo()
     {
-
+        System.out.println(returnType());
     }
+    public abstract String returnType();
+}
+
+class Carro extends Veiculos {
+
 }
