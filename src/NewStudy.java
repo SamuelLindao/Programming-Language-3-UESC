b0b347ee0ac18325f00fcaa1bc4be902dfe6cae7
SamuelLindao <lacerdasamuel26@gmail.com>
2025-06-02 18:35:19 -0300

    implemeted new class and study

--- src/NewStudy.java
+++ src/NewStudy.java
@@ -1,2 +1,22 @@
-﻿public class NewStudy {
+﻿public abstract class NewStudy {
+    int value = 0;
+    NewStudy(int value) {
+        this.value = value;
+    }
+
+    //private abstract não é permitido
+    //public faz com que a classe possa ser modificado até mesmo em outros pacotes
+    //eliminar public transforma em default, acessivel
+    abstract void ImprimirTipo();
 }
+
+class Study extends NewStudy {
+    Study(int value) {
+        super(value);
+    }
+
+    public void ImprimirTipo() {
+        System.out.println(value);
+    }
+}
+
